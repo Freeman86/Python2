26b48fab234f9cdcc7ef1d54d179a485ed96a0be
Freeman86 <33635798+Freeman86@users.noreply.github.com>
2019-04-26 19:29:16 +0300

    Create task_3.py

---
+++ task_3.py
@@ -0,0 +1,11 @@
+﻿a = int(input("Введите число: "))
+
+def recursion(a):
+    if (a // 10) == 0:
+        print(a)
+    else:
+        print(a % 10)
+        a = a // 10
+        recursion(a)
+
+recursion(a)
